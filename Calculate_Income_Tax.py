46d84d28fe7bd660f4af72cc58839e2ce94ae6dd
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-24 23:18:01 +0800

    Defined the function to calculate the income tax

--- Calculate_Income_Tax.py
+++ Calculate_Income_Tax.py
@@ -3,6 +3,13 @@
 # pseudocode
 
 # Define the function to calculate the income tax
-
+def calculate_income_tax(income):
+    if income <=10000:
+        tax=0
+    elif income <=20000:
+        tax=(income-10000)*0.1
+    else:
+        tax=10000*0.1+(income-20000)*0.2
+        return tax
 
 # Print the calculated income tax
